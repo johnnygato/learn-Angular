1d6a107545e279323b7a7802ecf7cc9a98a7cdaa
Unknown <joao88vsc@gmail.com>
2017-05-02 15:42:52 +0100

    Clean Up, created a reusable component that accept an input

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-
-export class Hero {
-  id: number;
-  name: string;
-}
+import { Hero } from './hero';
 
 const HEROES: Hero[] = [
-  {id:11, name: 'Mr.Nice'},
+  { id: 11, name: 'Mr.Nice'},
   { id: 12, name: 'Narco' },
   { id: 13, name: 'Bombasto' },
   { id: 14, name: 'Celeritas' },
@@ -30,16 +26,9 @@ const HEROES: Hero[] = [
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
-    
-    `,
+    <hero-detail [hero]="selectedHero"></hero-detail>
+    `
+    ,
   styles: [`
   .selected {
     background-color: #CFD8DC !important;
@@ -95,7 +84,7 @@ export class AppComponent {
   title = 'Tour of Heroes';
   heroes = HEROES;
   selectedHero: Hero;
- 
+  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
